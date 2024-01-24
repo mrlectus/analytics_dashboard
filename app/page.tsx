704b2b18ff3cdfb68557fff29d432da2a3e192d4
BrownCarter <lilbrown3000@gmail.com>
2024-01-25 00:15:58 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { TopPlatform } from "@/components/top-platform";
 
 export default function Home() {
   return (
-    <div className="flex flex-row gap-4 p-4 justify-evenly">
+    <div className="flex flex-col lg:flex-row gap-4 p-4 justify-evenly">
       <div className="flex flex-col gap-4">
         <SalesTrend />
         <LastOrder />
